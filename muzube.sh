4f2b7293daf2f8ed13f0b822579ca6db30def929
Thomas De Backer <thomasisdebacker5@gmail.com>
2015-11-17 16:24:57 +0100

    more artists

--- muzube.sh
+++ muzube.sh
@@ -14,15 +14,15 @@ then
 fi
 
 #make folder if not exists
-mkdir -p songs
+mkdir -p msssongs
 #go into folder
-cd songs
+cd msssongs
 
 #download all the songs!
 echo Downloading songs
 youtube-dl --dateafter $LastDownloaded --playlist-end $SongLimit \
 	--max-filesize 50m --extract-audio --audio-format $Format \
-	--output "%(title)s.%(ext)s" $Playlist
+	--output "%(title)s.%(ext)s" $mssPlaylist
 
 #give all the songs the right tags
 echo Tagging songs
@@ -32,7 +32,7 @@ do
 	then
 		ARTIST=$(echo $i | sed 's/\(.*\) - .*\.mp3/\1/')
 		SONG=$(  echo $i | sed 's/.* - \(.*\)\.mp3/\1/')
-		ALBUM=$Album
+		ALBUM=$mssAlbum
 		id3v2 --artist="$ARTIST" --album="$ALBUM" --song="$SONG" "$i"
 	fi
 done
@@ -40,10 +40,54 @@ echo end tagging
 #return a folder
 cd ..
 #upload songs to google music
-./gmusicapi-scripts/gmupload.py --uploader-id $MacAdress --match songs/*
+./gmusicapi-scripts/gmupload.py --uploader-id $MacAdress --match msssongs/*
 
 #remove uploaded songs
-rm songs/*
+rm msssongs/*
+
+#-----------xKito music-----------------------
+
+#make folder if not exists
+mkdir -p xksongs
+mkdir -p mislukt
+#go into folder
+cd xksongs
+
+#download all the songs!
+echo Downloading songs
+youtube-dl --dateafter $LastDownloaded --playlist-end $SongLimit \
+	--max-filesize 50m --extract-audio --audio-format $Format \
+	--output "%(title)s.%(ext)s" $xkPlaylist
+
+#give all the songs the right tags
+echo Tagging songs
+for i in *
+do
+	if test -f "$i"
+	then
+		if echo $i | grep --quiet "【.*】.*-.*" 
+		then
+		
+		ARTIST=$(echo $i | sed 's/【\(.*\)】\(.*\) - \(.*\)/\2/')
+		SONG=$(  echo $i | sed 's/【\(.*\)】\(.*\) - \(.*\)/\3/')
+		GENRE=$( echo $i | sed 's/【\(.*\)】\(.*\) - \(.*\)/\1/')
+		ALBUM=$xkAlbum
+		id3v2 --artist="$ARTIST" --album="$ALBUM" --song="$SONG" --genre="$GENRE" "$i"
+		else
+			echo "geen geldig liedje"
+			mv $i ../mislukt/
+		fi
+	fi
+done
+echo end tagging
+#return a folder
+cd ..
+#upload songs to google music
+./gmusicapi-scripts/gmupload.py --uploader-id $MacAdress --match xksongs/*
+
+#remove uploaded songs
+rm xksongs/*
+
 
 #update date
 sed -i -e "s/LastDownloaded=.*/LastDownloaded=$(date +%Y%m%d)/" muzube.conf
